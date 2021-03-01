82a8ab897848a3ddb21c2aef3959603d93374f7f
Neud Tesfay <kingneud55@gmail.com>
2021-03-01 18:54:14 +0300

    Coming soon page

---
+++ resources/views/layout/indexPage.blade.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="ThemeStarz">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600">
+        <link rel="stylesheet" href="/icons/font-awesome/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="/css/style.css">
+
+        <!-- Vendors -->
+        <link rel="stylesheet" href="/vendors/magnific-popup.css">
+        <link rel="stylesheet" href="/vendors/bootstrap/css/bootstrap.min.css" type="text/css">
+        <title>mhooronline — Coming Soon</title>
+
+    </head>
+    <body class="has-loading-screen ts-theme-light">
+
+        <!--*************************************************************************************************************-->
+        <!--************ MAIN SCREEN ************************************************************************************-->
+        <!--*************************************************************************************************************-->
+
+        <div class="ts-page-wrapper">
+            <div class="container">
+
+                <!--NAVIGATION ******************************************************************************************-->
+                <header id="header">
+                    <nav class="navbar navbar-light ts-separate-bg-element">
+                        <a class="navbar-brand" href="#">
+                            <img src="assets/img/logo-b.png" alt="">
+                        </a>
+                        <!--end navbar-brand-->
+                        {{-- <button class="navbar-toggler ts-open-side-panel" type="button" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button> --}}
+                    </nav>
+                    <!--end navbar-->
+                </header>
+                <!--end #header-->
+
+                <!--CONTENT *********************************************************************************************-->
+                <main id="main-content">
+                    <div class="ts-content-wrapper">
+                        <div class="row">
+                            <!--Page Title-->
+                            <div class="col-md-6">
+                                <h1 class="colored-text">Get Ready For Our<br>Launch</h1>
+                                <p class="colored-text">A distance education for primary and secondary school: grade 1-12 learn via mhooronline.com and get certified from the leading schools.</p>
+                                {{-- <a href="#" class="btn btn-primary mr-2 ts-open-side-panel">Learn More</a> --}}
+                                {{-- <a href="https://vimeo.com/63502573" class="btn btn-outline-dark video-popup"><i class="fa fa-play mr-2"></i>Video</a> --}}
+                            </div>
+                            <!--end col-md-6-->
+                            <!--Count Down-->
+                            <div class="col-md-6">
+                                <div class="text-center my-4">
+                                    <div class="ts-count-down colored-text" data-date="May 1, 2021 15:03:26"></div>
+                                    <!--end ts-count-down-->
+                                </div>
+                            </div>
+                            <!--end col-md-6-->
+                        </div>
+                        <!--end row-->
+                    </div>
+                    <!--end ts-content-wrapper-->
+                </main>
+                <!--end #main-content-->
+
+                <!--FOOTER **********************************************************************************************-->
+                <footer id="footer">
+                    <div class="clearfix py-4">
+                        <div class="float-none float-sm-left">
+                            <div class="ts-social-icons">
+                                <a href="#" class="fab fa-facebook"></a>
+                                <a href="#" class="fab fa-twitter"></a>
+                                <a href="#" class="fab fa-youtube"></a>
+                                <a href="#" class="fab fa-instagram"></a>
+                            </div>
+                        </div>
+                        <!--end social-icons-->
+                        <div class="float-none float-sm-right text-left text-sm-right ts-opacity__50">
+                            <small class="colored-text">Copyright &copy; 2021 mhooronline.com, All Rights Reserved</small>
+                        </div>
+                        <!--end text-left-->
+                    </div>
+                    <!--end clearfix-->
+                </footer>
+                <!--end #footer-->
+            </div>
+            <!--end container-->
+
+            <!--BACKGROUND **********************************************************************************************-->
+            <div class="ts-background ts-shapes-canvas position-fixed ts-separate-bg-element" data-bg-color="#C1FF00">
+                {{-- <div class="ts-background-image ts-animate ts-scale" data-bg-image="/images/bg/14.png" data-bg-opacity=".1"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/13.png"></div>
+                <div class="ts-background-image ts-animate ts-scale" data-bg-image="/images/bg/12.png" data-bg-opacity=".3"></div>
+                <div class="ts-background-image ts-animate ts-scale" data-bg-image="/images/bg/11.png"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/10.png"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/9.png" data-bg-opacity=".8"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/8.png" data-bg-opacity=".8"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/7.png"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/6.png" data-bg-opacity=".8"></div>
+                <div class="ts-background-image ts-animate" data-bg-image="/images/bg/5.png" data-bg-opacity=".8"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/4.png"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/3.png" data-bg-opacity=".8"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/2.png"></div>
+                <div class="ts-background-image ts-animate ts-bounce" data-bg-image="/images/bg/1.png" data-bg-opacity=".8"></div> --}}
+            </div>
+            <!--end ts-background-->
+        </div>
+
+        <script src="http://maps.google.com/maps/api/js?key=AIzaSyBEDfNcQRmKQEyulDN8nGWjLYPm8s4YB58"></script>
+        
+        <script src="/js/jquery-3.3.1.min.js"></script>
+        <script src="/js/popper.min.js"></script>
+        <script src="/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/js/imagesloaded.pkgd.min.js"></script>
+        <script src="/js/jquery.magnific-popup.min.js"></script>
+        <script src="/js/jquery.countdown.min.js"></script>
+        <script src="/js/jquery.validate.min.js"></script>
+        <script src="/js/jquery-validate.bootstrap-tooltip.min.js"></script>
+        
+        <script src="/js/custom.js"></script>
+    </body>
+</html>
